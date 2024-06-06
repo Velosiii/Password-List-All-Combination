e37c66192f27edcbc37944b7d65054aab04c592a
Velosi <77051139+Velosiii@users.noreply.github.com>
2024-06-06 21:12:49 +0300

    Password_Generater.py

--- Password_Generater.py
+++ Password_Generater.py
@@ -1,9 +1,12 @@
 
 k = "0123456789qwertyuıopğüişlkjhgfdsazxcvbnmöçQWERTYUIOPĞÜİŞLKJHGFDSAZXCVBNMÖÇ~`!@#£€$¢¥§%°^&*()-_+={]}[|\"/:;\'><,.\\?"
 
+ths = open("3Letter.txt","w")
 for bas1 in k:
     for bas2 in k:
         for bas3 in k:
-            for bas4 in k:
-                password = (bas1+bas2+bas3+bas4)
-                print(password)
+            password = (bas1+bas2+bas3)
+            print(password)
+            ths.write(password+"\n")
+
+ths.close
